090a4fe9f7d7683df55d08b852b99debe0a87eb1
Boaz Marube <boazmarube1@gmail.com>
2025-06-21 12:35:55 +0300

    Added a theme-toggle icon on header

--- build/js/main.js
+++ build/js/main.js
@@ -11,5 +11,17 @@ const initApp = () => {
     hamburgerBtn.addEventListener('click', toggleMenu)
     mobileMenu.addEventListener('click', toggleMenu)
 }
+const themeToggle = document.getElementById('theme-toggle')
+themeToggle.addEventListener("click", ()=>{
+    const htmlElement =document.documentElement;
+    htmlElement.classList.toggle("dark");
+
+    if(htmlElement.classList.contains("dark")){
+        themeToggle.textContent = "☀️";
+    }else{
+        themeToggle.textContent =" 🌙";
+    }
+});
+
 
 document.addEventListener('DOMContentLoaded', initApp)
